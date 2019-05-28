a6058f3937db7ee97e1ce12e2c7d2b8717b317dc
Jameka Echols <jameka.echols@gmail.com>
2019-05-28 13:24:39 -0500

    finished the assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dictionaryWords
 {
@@ -6,7 +7,39 @@ namespace dictionaryWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Create a dictionary with key value pairs to
+            represent words (key) and its definition (value)
+            */
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            wordsAndDefinitions.Add("success", "The matter of never giving up.");
+            wordsAndDefinitions.Add("failure", "The act of giving up.");
+            wordsAndDefinitions.Add("happy", "A state of true contentment.");
+            wordsAndDefinitions.Add("love", "I don't know.");
+
+
+            /*
+                Use square bracket lookup to get the definition two
+                words and output them to the console
+            */
+            Console.WriteLine(wordsAndDefinitions["happy"]);
+            Console.WriteLine(wordsAndDefinitions["failure"]);
+
+
+            /*
+                Loop over dictionary to get the following output:
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+                Console.WriteLine($"The definition of {word.Key} is {word.Value}");
+            }
+            
         }
     }
 }

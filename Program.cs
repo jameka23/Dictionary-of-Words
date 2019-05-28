e65bcfbcbf36029c924664b82e70f69b8af589a6
Jameka Echols <jameka.echols@gmail.com>
2019-05-28 13:08:38 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dictionaryWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
